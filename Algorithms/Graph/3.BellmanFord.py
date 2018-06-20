75efa3b41aa2947a2e66499c4ff3671704a4c2c0
Alfonsxh <alfonsxh@gmail.com>
2018-06-20 21:20:19 +0800

    1、新增BellmanFord算法实现

--- Algorithms/Graph/3.BellmanFord.py
+++ Algorithms/Graph/3.BellmanFord.py
@@ -7,3 +7,45 @@
 @time: 18-6-18 下午4:55
 @version: v1.0 
 """
+from collections import namedtuple
+
+# 图
+graph = dict()
+graph["D"] = dict(B=1, C=5)
+graph["B"] = dict(C=3, D=2, E=2)
+graph["A"] = dict(B=-1, C=4)
+graph["E"] = dict(D=-3)
+
+# 开销
+infinity = float("inf")
+costs = dict(A=0, B=infinity, C=infinity, D=infinity, E=infinity)
+
+# 父节点
+parents = dict()
+
+EdgeType = namedtuple("edge", "U,V,len")
+
+
+def BellmanFord():
+    # 构造边集合
+    edgeList = list()
+    for U, value in graph.items():
+        for V, length in value.items():
+            edgeList.append(EdgeType(U=U, V=V, len=length))
+
+    # 算法主体
+    for i in range(len(graph.keys()) - 1):                      # 遍历顶点数-1次
+        for edge in edgeList:                                   # 遍历的所有的边集合
+            if costs[edge.U] + edge.len < costs[edge.V]:        # relaxa(松弛主体)
+                costs[edge.V] = costs[edge.U] + edge.len
+                parents[edge.V] = edge.U
+
+    # 回环判断
+    for edge in edgeList:
+        if costs[edge.U] + edge.len < costs[edge.V]:
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    print("It's have loopback？\n{answer}".format(answer="Yes!" if BellmanFord() else "No!"))
