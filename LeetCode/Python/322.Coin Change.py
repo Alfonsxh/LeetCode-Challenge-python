8309585ed913e11128d5213d4bbc74320867e4ed
Alfonsxh <xiaohuihui100@gmail.com>
2018-06-25 00:12:42 +0800

    1、LeetCode 322 518

--- LeetCode/Python/322.Coin Change.py
+++ LeetCode/Python/322.Coin Change.py
@@ -18,14 +18,13 @@ class Solution:
         :type amount: int
         :rtype: int
         """
-        d = dict()
-        d[0] = 0
+        d = {key: float("inf") for key in range(1, amount + 1)}     # 初始化币值所需最少币数字典，初始值为infity
+        d[0] = 0    # 0元需要的币数为0
 
-        for value in range(1, amount + 1):
-            d[value] = float("inf")
+        for value in range(1, amount + 1):          # 从1开始遍历所有币值
 
-            for coin in coins:
-                if value - coin >= 0 and d[value - coin] + 1 < d[value]:
+            for coin in coins:              # 遍历所有可选币值
+                if value - coin >= 0 and d[value - coin] + 1 < d[value]:   # 如果币值大于可选币值并且所用的币数更少，则更新对应的币数
                     d[value] = d[value - coin] + 1
         return d[amount] if d[amount] != float("inf") else -1
 
