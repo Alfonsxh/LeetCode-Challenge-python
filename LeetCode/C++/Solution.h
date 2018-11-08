8f07614c1267053ba83b24852d5be54cbe255900
Alfonsxh <alfonsxh@gmail.com>
2018-11-09 00:22:06 +0800

    1、新增27\80\283题代码

--- LeetCode/C++/Solution.h
+++ LeetCode/C++/Solution.h
@@ -12,14 +12,15 @@ using namespace std;
 
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
+    // 26. Remove Duplicates from Sorted Array
+    int removeDuplicates(vector<int> &nums) {
         vector<int> tmpVec;
 
         auto L = nums.begin(), R = nums.begin();
-        for (;  R != nums.end() ; R++) {
-            if (*L != *R){
+        for (; R != nums.end(); R++) {
+            if (*L != *R) {
                 tmpVec.push_back(*L);
-                L =R;
+                L = R;
             }
         }
 
@@ -27,7 +28,53 @@ public:
             tmpVec.push_back(*L);
 
         nums.swap(tmpVec);
-        return (int)nums.size();
+        return (int) nums.size();
+    }
+
+    // 80. Remove Duplicates from Sorted Array II
+    int removeDuplicates2(vector<int> &nums) {
+        vector<int> tmpVec;
+
+        int L = 0, R = 0;
+        for (; R < nums.size(); R++) {
+            if (nums[L] == nums[R] && R - L < 2) {
+                tmpVec.push_back(nums[L]);
+            }
+
+            if (nums[L] != nums[R]) {
+                tmpVec.push_back(nums[R]);
+                L = R;
+            }
+
+        }
+
+        nums.swap(tmpVec);
+        return (int) nums.size();
+    }
+
+    // 27. Remove Element
+    int removeElement(vector<int> &nums, int val) {
+        vector<int> tmpVec;
+
+        for (auto n : nums) {
+            if (n != val) {
+                tmpVec.push_back(n);
+            }
+        }
+
+        nums.swap(tmpVec);
+        return nums.size();
+    }
+
+    // 283. Move Zeroes
+    void moveZeroes(vector<int> &nums) {
+        int L = 0;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != 0) {
+                swap(nums[L++], nums[i]);
+            }
+        }
     }
 };
 
--- LeetCode/C++/main.cpp
+++ LeetCode/C++/main.cpp
@@ -10,8 +10,21 @@ int main() {
     Solution sol;
 
     // 26. Remove Duplicates from Sorted Array
-    std::vector<int> nums{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-    std::cout << "There have " << sol.removeDuplicates(nums) << " diff elements." << std::endl;
+    std::vector<int> nums1{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    std::cout << "There have " << sol.removeDuplicates(nums1) << " diff elements." << std::endl;
+
+    // 27. Remove Element
+    std::vector<int> nums2{2, 2};
+    std::cout << "There have " << sol.removeElement(nums2, 2) << " left elements." << std::endl;
+
+    // 283. Move Zeroes
+    std::vector<int> nums3{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    sol.moveZeroes(nums3);
+    std::cout << "The new nums is " << nums3.data() << std::endl;
+
+    // 80. Remove Duplicates from Sorted Array II
+    std::vector<int> nums4{0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 5, 5, 5, 5, 5,};
+    std::cout << "There have " << sol.removeDuplicates2(nums4) << " diff elements." << std::endl;
 
     return 1;
 }
